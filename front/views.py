8d5afa9f7eed9e7b58de14d35fc3787b79e737d0
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-06-13 10:02:42 +0200

    push

--- front/views.py
+++ front/views.py
@@ -6,4 +6,4 @@ from contact.models import Contact
 from testimonial.models import Testimonial
 
 def home(request):
-    return render(request, 'portdjango/front/index.html', {'about': About.objects.all()[0], 'skills': Skill.objects.all(), 'services': Services.objects.all(), 'contact': Contact.objects.all(), 'testimonials': Testimonial.objects.all()})
+    return render(request, 'portdjango/front/index.html', {'about': About.objects.all()[0], 'skills': Skill.objects.all(), 'services': Services.objects.all(), 'contact': Contact.objects.all()[0], 'testimonials': Testimonial.objects.all()})
